3ecd4ded48be2356f4837ba0c47a6657520b0ca3
jsovalles <jsovalles95@gmail.com>
2020-09-10 14:14:19 -0500

    initial commit

---
+++ src/main/java/com/avv/letters/LettersApplication.java
@@ -0,0 +1,13 @@
+package com.avv.letters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LettersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LettersApplication.class, args);
+	}
+
+}
